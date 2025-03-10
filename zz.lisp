f26086042244dc4b0e518f443073c0754c7391cc
lain <lain2_lmpkaur@Lain.>
2025-03-10 16:54:14 +0900

    queue-looplist

--- zz.lisp
+++ zz.lisp
@@ -1,9 +1,26 @@
-(defstruct (foo
-             (:CONSTRUCTOR build-foo
-                (&optional (x 1)
-                      &aux (a (* 10 x))
-                           (b (* 20 x))))) a b)
+(setf *PRINT-CIRCLE* t)
 
-(build-foo 100)
-(build-foo)
+(defstruct queue (rear nil))
+
+(defvar bar (make-queue))
+
+(defun emptyp (q)
+  (null (queue-rear q)))
+
+(defun enqueue (item q)
+  (let ((new-rear (list item)))
+    (if (emptyp q)
+        (setf (cdr new-rear) new-rear)
+        (setf (cdr new-rear) (cdr (queue-rear q))
+              (cdr (queue-rear q)) new-rear))
+    (setf (queue-rear q) new-rear)))
+
+(defun dequeue (q)
+  (unless (emptyp q)
+    (let ((front (cdr (queue-rear q))))
+      (if (eq front (queue-rear q))
+          (setf (queue-rear q) nil)
+          (setf (cdr (queue-rear q)) (cdr front)))
+      (car front))))
+
 
